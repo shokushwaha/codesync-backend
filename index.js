d6cc901f64c78e1f2d5af029ac0ec44dbd30d001
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-03-19 10:59:08 +0530

    second

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 require('dotenv').config()
 app.use(express.static('build'));
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
